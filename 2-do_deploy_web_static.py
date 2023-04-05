5c384d32875e3ef0ff5bcefb6db5db1db86342e3
Jay stones <Joelpaul345@gmail.com>
2023-04-05 23:34:24 +0100

    Static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,38 +9,38 @@ env.user = 'ubuntu'
 env.key_filename = '/root/.ssh/school'
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers
-    """
-    try:
-        if not os.path.exists(archive_path):
-            return False
-        # Upload the archive to the server
-        put(archive_path, "/tmp/")
+       """
+        Distributes an archive to your web servers
+        """
+        try:
+            if not os.path.exists(archive_path):
+                    return False
+            # Upload the archive to the server
+            put(archive_path, "/tmp/")
 
-        # Create the directory where the files will be extracted
-        web_static = archive_path.split("/")[-1]
-        web_static_path = web_static.split(".")[0]
-        run("sudo mkdir -p /data/web_static/\
+            # Create the directory where the files will be extracted
+            web_static = archive_path.split("/")[-1]
+            web_static_path = web_static.split(".")[0]
+            run("sudo mkdir -p /data/web_static/\
             releases/web_static_{}/".format(web_static_path))
-        # Extract the archive into the directory
-        run("sudo tar -xzf /tmp/web_static_{}.tgz -C /data/web_static\
+            # Extract the archive into the directory
+            run("sudo tar -xzf /tmp/web_static_{}.tgz -C /data/web_static\
             /releases/web_static_{}/".format(web_static, web_static_path))
-        # Delete the archive from the server
-        run("sudo rm /tmp/web_static_{}.tgz".format(web_static))
+            # Delete the archive from the server
+            run("sudo rm /tmp/web_static_{}.tgz".format(web_static))
 
-        # Move the contents of the web_static directory up one level
-        run("sudo mv /data/web_static/releases/{}/web_static/*\
+            # Move the contents of the web_static directory up one level
+            run("sudo mv /data/web_static/releases/{}/web_static/*\
             /data/web_static\
             /releases/{}/".format(web_static_path, web_static_path))
-        # Remove the now empty web_static directory
-        run("sudo rm -rf /data/web_static/releases/{} \
+            # Remove the now empty web_static directory
+            run("sudo rm -rf /data/web_static/releases/{} \
             /web_static_{}/web_static".format(web_static_path))
-        # Delete the symbolic link /data/web_static/current
-        run("sudo rm -rf /data/web_static/current")
-        # Create a symbolic link from /data/web_static/current to new code
-        run("sudo ln -s /data/web_static/releases/{}/ \
+            # Delete the symbolic link /data/web_static/current
+            run("sudo rm -rf /data/web_static/current")
+            # Create a symbolic link from /data/web_static/current to new code
+            run("sudo ln -s /data/web_static/releases/{}/ \
             /data/web_static/current".format(web_static_path))
         except:
-                return False
+            return False
         return True
