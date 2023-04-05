4d478017da8012a9579b4f544db3bde9b6df67b7
Jay stones <Joelpaul345@gmail.com>
2023-04-05 13:44:58 +0100

    Create 1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from fabric.api import local
+from time import strftime
+from datetime import datetime
+import os
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+    try:
+        # Create the versions directory if it doesn't exist
+        if not os.path.exists("versions"):
+            os.makedirs("versions")
+
+        # Create the name of the archive file
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(now)
+
+        # Create the archive using the tar command
+        local("tar -cvzf {} web_static".format(archive_path))
+
+        # Return the path to the archive file
+        return archive_path
+    except:
+        # Return None if an error occurred
+        return None
