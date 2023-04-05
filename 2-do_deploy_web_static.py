a0588eaa9be20af2ca450237508bb9733b8dc42e
Jay stones <Joelpaul345@gmail.com>
2023-04-06 00:26:58 +0100

    static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,45 +10,26 @@ env.key_filename = '/root/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """
-    Deploy web files to server
-    """
-    try:
-        if not (path.exists(archive_path)):
-            return False
-
-        # upload archive
-        put(archive_path, '/tmp/')
-
-        # create target dir
-        web_static = archive_path.split("/")[-1]
-        web_static_path = web_static.split(".")[0]
-        run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(web_static_path))
-
-        # uncompress archive and delete .tgz
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'.format(web_static, web_static_path))
-
-        # remove archive
-        run('sudo rm /tmp/web_static_{}.tgz'.format(web_static))
-        # move contents into host web_static
-        run('sudo mv /data/web_static/releases/web_static_{}\
-/web_static/* /data/web_static/releases\
-/web_static_{}/'.format(web_static_path, web_static_path))
-
-        # remove extraneous web_static dir
-        run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'.format(web_static_path))
-
-        # delete pre-existing sym link
-        run('sudo rm -rf /data/web_static/current')
-
-        # re-establish symbolic link
-        run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(web_static_path))
-    except Exception as e:
+    """Distributes an archive to web servers"""
+    if not path.exists(archive_path):
         return False
 
-    # return true on success
+    # Upload the archive to the /tmp/ directory of the web server
+    put(archive_path, "/tmp/")
+
+    # Uncompress the archive to /data/web_static/releases/
+    archive_filename = archive_path.split("/")[-1]
+    archive_basename = archive_filename.split(".")[0]
+    release_dir = "/data/web_static/releases/" + archive_basename
+    run("mkdir -p {}".format(release_dir))
+    run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_dir))
+    run("rm /tmp/{}".format(archive_filename))
+    run("mv {}/web_static/* {}/".format(release_dir, release_dir))
+    run("rm -rf {}/web_static".format(release_dir))
+
+    # Update the symbolic link
+    current_dir = "/data/web_static/current"
+    run("rm -f {}".format(current_dir))
+    run("ln -s {} {}".format(release_dir, current_dir))
+
     return True
