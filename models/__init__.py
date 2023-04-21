c1ae40762d6a524ed03bb0497e6f07c8b70a19d7
Jaaystones <Joelpau345@gmail.com>
2023-04-21 07:58:28 -0700

    AirBnB

--- models/__init__.py
+++ models/__init__.py
@@ -5,11 +5,8 @@ from os import getenv
 
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
-if storage_type == 'db':
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
+def __init__(self, *args, **kwargs):
+    """Initializes State instance"""
+    super().__init__(*args, **kwargs)
 
 storage.reload()
