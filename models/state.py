6b50ac4f1473a15ef6f7f0b517a74ac0934a861d
Jaaystones <Joelpau345@gmail.com>
2023-04-21 06:40:27 -0700

    AirBnB

--- models/state.py
+++ models/state.py
@@ -5,15 +5,15 @@ from models import storage_type
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from os import getenv
 
 class State(BaseModel, Base):
     """ State class / table model"""
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-    __tablename__ = 'states'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
+        __tablename__ = 'states'
+        if storage_type == 'db':
+            name = Column(String(128), nullable=False)
+            cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
     else:
         name = ''
