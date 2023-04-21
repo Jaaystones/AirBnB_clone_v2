e5a578c36ae85d970fb8c01f920b4decdc5c479a
Jay stones <Joelpaul345@gmail.com>
2023-04-21 16:06:05 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -11,8 +11,8 @@ from models import storage
 
 class State(BaseModel):
     """State class"""
+    __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship('City', cascade='all, delete', backref='state')
     else:
