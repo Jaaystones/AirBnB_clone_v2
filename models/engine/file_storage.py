9bd7f3ec533c205b59c103007a31eefd9bffce1d
Jay stones <Joelpaul345@gmail.com>
2023-04-21 22:02:27 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,49 +5,30 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-
-    __file_path = "file.json"
+    __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None) -> dict:
-        """Returns a dictionary of models of specified class
-        currently in storage
+    def all(self, cls=None):
         """
-        from models.base_model import Base
-        from models.amenity import Amenity
-        from models.city import City
-        from models.place import Place
-        from models.review import Review
-        from models.state import State
-        from models.user import User
-
-        if cls:
+        Returns a dictionary of models currently in storage.
+        Update that returns the list of objects of one type of class (optional)
+        """
+        if cls is None:
+            return FileStorage.__objects
+        else:
             cls_dict = {}
-            if isinstance(cls, str):
-                cls = eval(cls)
-            cls_name = cls.__name__
-            all_objs = FileStorage.__objects
-            for key, val in all_objs.items():
-                if key.split(".")[0] == cls_name:
-                    cls_dict.update({key: val})
+            for key, obj_val in FileStorage.__objects.items():
+                if isinstance(obj_val, cls):
+                    cls_dict[key] = obj_val
             return cls_dict
-        return FileStorage.__objects
-
-    def delete(self, obj=None):
-        """Deletes object instance from local storage FileStorage.__objects"""
-        if obj is None:
-            return
-        obj_key = "{}.{}".format(type(obj).__name__, obj.id)
-        del FileStorage.__objects[obj_key]
-        self.save()
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()["__class__"] + "." + obj.id: obj})
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, "w") as f:
+        with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
@@ -63,24 +44,32 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
-
-        classes = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "Place": Place,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Review": Review,
-        }
+        classes = {'BaseModel': BaseModel,
+                   'User': User,
+                   'Place': Place,
+                   'State': State,
+                   'City': City,
+                   'Amenity': Amenity,
+                   'Review': Review}
         try:
             temp = {}
-            with open(FileStorage.__file_path, "r") as f:
+            with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val["__class__"]](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
+    def delete(self, obj=None):
+        """Method to delete obj from __objects if it’s inside"""
+        if obj is not None:
+            obj_cls = str(obj).split(" ")[0]
+            obj_cls = obj_cls[1:-1]
+            key = obj_cls + "." + obj.id
+            if key in FileStorage.__objects:
+                del(self.all()[key])
+                self.save()
+
     def close(self):
-        self.reload()
+        """ call reload() method for deserializing the JSON file to objects"""
+        reload()
