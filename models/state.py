1ea1c9ba7140e59cf780fea4d03fbb056d989eb3
Jay stones <Joelpaul345@gmail.com>
2023-04-21 14:42:52 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models import storage_type
+from models.base_model import BaseModel
 from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from os import getenv
 
-class State(BaseModel, Base):
-    """ State class / table model"""
+
+class State(BaseModel):
+    """State class"""
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'states'
-        if storage_type == 'db':
-            name = Column(String(128), nullable=False)
-            cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
     else:
-        name = ''
+        name = ""
 
         @property
         def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
+            """Getter attribute that returns the list of City instances
+            with state_id == current State.id"""
             from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+            cities_dict = storage.all(City)
+            return [city for city in cities_dict.values() if city.state_id == self.id]
